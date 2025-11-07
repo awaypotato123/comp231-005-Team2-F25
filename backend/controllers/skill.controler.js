de7db91bf78921318275af3a07dcc4e51d12d08d
christian <christiankhan100@gmail.com>
2025-11-07 16:41:53 -0500

    add skill controller with getAllSkills function

---
+++ backend/controllers/skill.controler.js
@@ -0,0 +1,23 @@
+import Skill from "../models/Skill";
+import User from "../models/User";
+
+export const getAllSkills = async (req, res) => {
+    try {
+        const {keyword, category} = req.query;
+
+        if(keyword) {
+            query.title = { $regex: keyword, $options: "i"};
+        }
+
+        if(category) {
+            query.category = category;
+        }
+
+        const skills = await Skill.find(query).populate("userId", "firstName lastName email");
+        res.status(200).json(skills);
+    }
+    catch(err) {
+        console.error("Error fetching skills", err);
+        res.status(500).json({ message: "Server error fetching skills"});
+    }
+}
