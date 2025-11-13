77a615f7b68787f40ba72778bebe3f24e36026fc
christian <christiankhan100@gmail.com>
2025-11-12 22:48:59 -0500

    add separate search feature in search controller

---
+++ backend/controllers/search.controller.js
@@ -0,0 +1,16 @@
+import Skill from "../models/Skill.js";
+import User from "../models/User.js";
+
+export const search = async (req, res) => {
+    try {
+        const query = req.query.query;
+        const skills = await Skill.find({ name: { $regex: query, $options: "i" } });
+        const user = await User.find({ name: { $regex: query, $options: "i" } });
+
+        res.json({ skills, teachers });
+    }
+    catch (err) {
+        console.error("Error fetching results: ", err);
+        res.status(500).json({ message: "Server error fetching results"});
+    }
+}
